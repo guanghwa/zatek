44f4cdf909d23f425043945bfce828a2f9bca314
guanghwa <guanghwa26@gmail.com>
2024-11-26 00:33:20 +0300

    register current state

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 // Import your components
-import Home from './views/Home.vue';
+import Home from './pages/Home.vue';
 // import About from './views/About.vue';
 // import Services from './views/Services.vue';
 // import Contact from './views/Contact.vue';
