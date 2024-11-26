9c3e8d67db39b6ad9f722b465d35f49b1417fdad
guanghwa <guanghwa26@gmail.com>
2024-11-26 14:49:42 +0300

    implement landing

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,27 @@
 <template>
-    <div class="pt-24 px-10 xl:px-24 2xl:px-44 ">
-        <p class="text-7xl pb-5">Zatek <br /> Software Business</p>
-        <p class="text-2xl text-gray-600"> Elevate your Business with AI Solutions from Zatek </p>
-        <Button class="py-2.5 px-5 text-lg w-56 h-14 bg-red-400 mt-10 mb-2.5"> Contact </Button>
+    <div class="px-10">
+        <div class="pt-24 xl:px-24 2xl:px-44 ">
+            <p class="text-7xl pb-5">Zatek <br /> Software Business</p>
+            <p class="text-2xl text-gray-600"> Elevate your Business with AI Solutions from Zatek </p>
+            <Button class="py-2.5 px-5 text-lg w-56 h-14 bg-red-400 mt-10 mb-2.5"> Contact </Button>
+        </div>
+        <div class="py-25 flex justify-center items-center 2xl:gap-40 xl:gap-32">
+            <div class="flex gap-20 justify-center">
+                <div class="w-44">
+                    <p class="text-5xl font-bold pb-2.5"> 50+</p>
+                    <p class="text-xl text-gray-600">Global Partners</p>
+                </div>
+                <div class="w-44">
+                    <p class="text-5xl font-bold pb-2.5"> 10K+</p>
+                    <p class="text-xl text-gray-600">Retail Clients</p>
+                </div>
+            </div>
+            <div class="pb-2.5">
+                <p class="text-6xl font-bold">Our Impressive <br /> Experience</p>
+            </div>
+        </div>
     </div>
 </template>
 <script setup lang="ts">
-    import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 </script>
