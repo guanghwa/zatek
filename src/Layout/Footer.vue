ab74900e546ee50c44e35a450411c10387e79ad3
guanghwa <guanghwa26@gmail.com>
2024-11-26 13:49:27 +0300

    add footer

--- src/Layout/Footer.vue
+++ src/Layout/Footer.vue
@@ -1,8 +1,10 @@
 <template>
-    <div>
-        Footer
+    <div class="pt-12 px-4 pb-7">
+        <div class="pt-12">
+            <p class="font-medium text-2xl">Zatek by software LLC</p>
+        </div>
+        <div class="pt-12">
+            <p class="font-medium text-sm">© Copyright 2024, All Rights Reserved</p>
+        </div>
     </div>
 </template>
-
-<script setup lang="ts">
-</script>
