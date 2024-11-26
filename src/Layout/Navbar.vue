25604e8db18d3eec4ad7258570aadaeb3184700f
guanghwa <guanghwa26@gmail.com>
2024-11-26 17:28:09 +0300

    update content

--- src/Layout/Navbar.vue
+++ src/Layout/Navbar.vue
@@ -7,9 +7,11 @@ import {
 </script>
 
 <template>
-    <Menubar class="justify-between">
+    <Menubar class="justify-between bg-transparent absolute">
         <MenubarMenu>
-            <MenubarTrigger class="!text-2xl">Zatek</MenubarTrigger>
+            <MenubarTrigger class="p-0">
+                <img src="@/assets/Logo-ZaTek.jpeg" class="w-20 h-11 rounded-sm" />
+            </MenubarTrigger>
         </MenubarMenu>
         <MenubarMenu>
             <MenubarTrigger class="!px-4 !py-2.5 text-md bg-red-400 text-white">Contact</MenubarTrigger>
