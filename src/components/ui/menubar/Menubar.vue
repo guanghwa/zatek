af59cba7e660f5e60f32f595ff9b5bcfe474816d
guanghwa <guanghwa26@gmail.com>
2024-11-26 05:51:35 +0300

    implement navbar

--- src/components/ui/menubar/Menubar.vue
+++ src/components/ui/menubar/Menubar.vue
@@ -25,7 +25,7 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
     v-bind="forwarded"
     :class="
       cn(
-        'flex h-10 items-center gap-x-1 rounded-md border bg-background p-1',
+        'flex items-center gap-x-1 rounded-md bg-background py-5 px-4 w-full',
         props.class,
       )
     "
