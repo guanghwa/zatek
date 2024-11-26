2c31b0fc7dcd1dc3205da87d866afc7041776e39
guanghwa <guanghwa26@gmail.com>
2024-11-26 14:14:45 +0300

    landing text

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,10 @@
 <template>
-    <div>
-        HOME
+    <div class="pt-24 px-10 xl:px-24 2xl:px-44 ">
+        <p class="text-7xl pb-5">Zatek <br /> Software Business</p>
+        <p class="text-2xl text-gray-600"> Elevate your Business with AI Solutions from Zatek </p>
+        <Button class="py-2.5 px-5 text-lg w-56 h-14 bg-red-400 mt-10 mb-2.5"> Contact </Button>
     </div>
 </template>
 <script setup lang="ts">
+    import { Button } from "@/components/ui/button";
 </script>
